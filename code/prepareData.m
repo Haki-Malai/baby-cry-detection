93b6a4335899e4c6bcd9f2a0ac29c3929275a82c
rawmann <55710426+rawmann@users.noreply.github.com>
2022-04-24 23:58:07 +0300

    Update prepareData.m

--- code/prepareData.m
+++ code/prepareData.m
@@ -3,6 +3,6 @@ clf;
 
 % Extract melspectogram images of ESC-50 dataset
 
-sourceFolder = fullfile('./ESC-50);
+sourceFolder = fullfile('./ESC-50');
 targetFolder = fullfile('ESC-50-MelSpec/');
 ads = audioDatastore(sourceFolder, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
