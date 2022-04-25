4b0185ca865ff9cee4d22a6edd5bd1f9ca8ebdef
rawmann <55710426+rawmann@users.noreply.github.com>
2022-04-25 09:40:23 +0300

    Data preloading complete

--- code/prepareData.m
+++ code/prepareData.m
@@ -8,3 +8,36 @@ targetFolder = fullfile('ESC-50-MelSpec/');
 ads = audioDatastore(sourceFolder, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
 
 % Read audio and get mel spectogram data of the audio
+while hasdata(ads)
+  % Read audio
+  [audioInput, info] = read(ads);
+  newFileName = strrep(info.FileName, 'ESC-50', 'ESC-50-MelSpec');
+  
+  if exist(newFileName, "file")
+    continue;
+  end
+  
+  % Generate mel spectogram of the audio data
+  [S, cF, t] = melSpectrogram(audioInput, info.SampleRate);
+  
+  % Convert dB for plotting
+  s = 10*log10(S+eps);
+  
+  % Show melspectogram image
+  ah = axes('Units', 'normalized', 'Position', [0 0 1 1]);
+  for j = 1:size(S, 3)
+    figure(j);
+    surf(t, cF, S(:,:,j), 'EdgeColor', 'none');
+    view([0, 90])
+    axis(ah, 'tight')
+  end
+  drawnow;
+  
+  filedir = fileparts(newFileName);
+  if ~exists(filedir, 'dir')
+    mkdir(filedir);
+  end
+  
+  % Save image to 'ESC-50-MelSpec' folder
+  imwrite(getframe(gcf).cdata, fullfule(newFileName));
+end
